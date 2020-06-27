25b8795d33ec483375d98c30884caeb6c83a2f4e
Lanfig <orlandoaf@gmail.com>
2020-06-26 21:44:38 -0300

    Classe Video, REPL

---
+++ src/bd.kt
@@ -0,0 +1,13 @@
+
+data class Video(val nome : String, val ehPublico : Boolean, val visualizacoes : Int, val joias : Int)
+
+/*
+Salve na pasta src
+
+Para testar no REPL
+1. Build > Build Project
+2. Tools > Kotlin > Kotlin REPL
+3. Na área de texto, digite: Video("Dark", true, 145, 45)
+4. Continue: res0.nome
+
+ */
